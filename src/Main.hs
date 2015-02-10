41f6e83366dd61a51a3ffa1b143a4f4d2ab6e319
coelhudo <coelhudo@gmail.com>
2015-02-10 00:08:53 -0500

    Improving memory, we are not goint to load all the values from csv at once. Instead will be as streaming

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,21 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Vector as V
+import qualified Data.Foldable as F
 
-import Data.Csv --from cassava
+import Data.Csv.Streaming --from cassava
 
 type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
 
 main :: IO ()
 main = do
   csvData <- BL.readFile "batting.csv"
-  let summed = fmap (V.foldr summer 0) (baseballStats csvData)
+  let summed = F.foldr summer 0 (baseballStats csvData)
   putStrLn $ "Total atBats was " ++ (show summed)
     where summer = (+) . fourth
 
 fourth :: (a,b,c,d) -> d
 fourth (_, _, _, d) = d
 
-baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
+baseballStats :: BL.ByteString -> Records BaseballStats
 baseballStats = decode NoHeader
